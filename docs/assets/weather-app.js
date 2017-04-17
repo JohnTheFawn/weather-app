c233bd1a2800e15cd4306837681f72babc2e870e
John Mitchell <jmitchell@sca-corp.com>
2017-04-17 00:54:55 -0400

    and build files

--- docs/assets/weather-app.js
+++ docs/assets/weather-app.js
@@ -1968,7 +1968,7 @@ catch(err) {
 /* jshint ignore:start */
 
 if (!runningTests) {
-  require("weather-app/app")["default"].create({"googleKey":"AIzaSyBrZlgrOTHy6vGIwQ19xANWa92AO40JHMI","openWeatherMapKey":"f681c0cbbc732f9028667a2fdf706846","name":"weather-app","version":"0.0.0+9c9f564a"});
+  require("weather-app/app")["default"].create({"googleKey":"AIzaSyBrZlgrOTHy6vGIwQ19xANWa92AO40JHMI","openWeatherMapKey":"f681c0cbbc732f9028667a2fdf706846","name":"weather-app","version":"0.0.0+190074d7"});
 }
 
 /* jshint ignore:end */
