b9cc91e67588940d983d6c06924eaeb16ba07cb9
John Mitchell <jmitchell@sca-corp.com>
2017-04-17 00:51:38 -0400

    manually edited the built index file but why D:

--- docs/assets/weather-app.js
+++ docs/assets/weather-app.js
@@ -1968,7 +1968,7 @@ catch(err) {
 /* jshint ignore:start */
 
 if (!runningTests) {
-  require("weather-app/app")["default"].create({"googleKey":"AIzaSyBrZlgrOTHy6vGIwQ19xANWa92AO40JHMI","openWeatherMapKey":"f681c0cbbc732f9028667a2fdf706846","name":"weather-app","version":"0.0.0+bd68b3f2"});
+  require("weather-app/app")["default"].create({"googleKey":"AIzaSyBrZlgrOTHy6vGIwQ19xANWa92AO40JHMI","openWeatherMapKey":"f681c0cbbc732f9028667a2fdf706846","name":"weather-app","version":"0.0.0+9c9f564a"});
 }
 
 /* jshint ignore:end */
