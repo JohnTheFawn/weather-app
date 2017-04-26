e95940313339be360c2e038f17b68758fc960404
John Mitchell <johnthefawn91@gmail.com>
2017-04-26 14:43:48 -0400

    background image should now update without refreshing the page

--- app/helpers/background-image-by-time.js
+++ app/helpers/background-image-by-time.js
@@ -1,26 +1,29 @@
 import Ember from 'ember';
 
 export function backgroundImageByTime(params) {
-  var currentTime = new Date();
-  var hours = currentTime.getHours();
-  if(hours < 5){
-    return 'background-image-night';
+  var currentTime = params[0];
+  if(currentTime){
+    var hours = currentTime.getHours();
+    if(hours < 5){
+      return 'background-image-night';
+    }
+    else if(hours < 8){
+      return 'background-image-morning';
+    }
+    else if(hours < 13){
+      return '';
+    }
+    else if(hours < 18){
+      return 'background-image-day';
+    }
+    else if(hours < 20){
+      return 'background-image-sunset';
+    }
+    else{
+      return 'background-image-night';
+    }
   }
-  else if(hours < 8){
-    return 'background-image-morning';
-  }
-  else if(hours < 13){
-    return '';
-  }
-  else if(hours < 18){
-    return 'background-image-day';
-  }
-  else if(hours < 20){
-    return 'background-image-sunset';
-  }
-  else{
-    return 'background-image-night';
-  }
+  return '';
 
 }
 
